eefe93e8d7f2020c51e24b72270d4ef5b8968ec3
KramerEd <danat7531@gmail.com>
2022-06-15 16:53:47 +0300

    add reset timer

--- src/features/Timer.jsx
+++ src/features/Timer.jsx
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Vibration } from "react-native";
 import { Countdown } from "../components/CountDown";
 import { ProgressBar } from "react-native-paper";
+import Timing from "./Timing";
 
 import { RoundedButton } from "../components/RoundedButton";
 
-export const Timer = ({ focusSubject }) => {
+const ONE_SECOND_IN_MS = 1000;
+const PATTERN = [3 * ONE_SECOND_IN_MS];
+
+export const Timer = ({ focusSubject, clearSubject }) => {
 	const [isStarted, setIsStarted] = useState(false);
 	const [progress, setProgress] = useState(1);
+	const [minutes, setMinutes] = useState(0.1);
+
+	const onEnd = (reset) => {
+		Vibration.vibrate(PATTERN);
+		setIsStarted(false);
+		setProgress(1);
+		reset();
+	};
 
 	return (
 		<View style={styles.container}>
 			<View style={styles.countDown}>
 				<Countdown
+					minutes={minutes}
 					isPaused={!isStarted}
 					onProgress={(progress) => setProgress(progress)}
-					onEnd={() => {}}
+					onEnd={onEnd}
 				/>
 				<View style={{ paddingTop: 40 }}>
 					<Text style={styles.title}>Focusing on:</Text>
@@ -27,6 +40,16 @@ export const Timer = ({ focusSubject }) => {
 					progress={progress}
 					color="#5e84e2"
 					style={{ height: 10, borderRadius: 20 }}
+				/>
+			</View>
+			<View style={styles.buttonWrapper}>
+				<Timing onChangeTime={setMinutes} />
+			</View>
+			<View style={styles.clear}>
+				<RoundedButton
+					size={50}
+					title="-"
+					onPress={() => clearSubject()}
 				/>
 			</View>
 			<View style={styles.buttonWrapper}>
@@ -52,7 +75,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 	},
 	countDown: {
-		flex: 0.5,
+		flex: 0.4,
 		alignItems: "center",
 		justifyContent: "center",
 	},
@@ -61,6 +84,10 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		justifyContent: "center",
 		alignItems: "center",
+	},
+	clear: {
+		flexDirection: "row",
+		justifyContent: "center",
 	},
 	title: {
 		color: "white",
